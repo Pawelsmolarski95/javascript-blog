a654db16124a05f4c5ca8289af382ff8573bbca6
Pawel Smolarski <smolenc1@gmail.com>
2022-08-09 21:26:43 +0200

    javscript-blog

--- js/script.js
+++ js/script.js
@@ -4,33 +4,33 @@
 
   
 const titleClickHandler = function(event){
+    event.preventDefault();
     const clickedElement = this;
-    console.log('Link was clicked!');
-
+    
     /* [DONE] remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
     for(let activeLink of activeLinks){
     activeLink.classList.remove('active');
   }
-    /* add class 'active' to the clicked link */
+    /* [DONE] add class 'active' to the clicked link */
     clickedElement.classList.add('active');
     
 
     /* remove class 'active' from all articles */
     const activeArticles = document.querySelectorAll('.posts .active');
-    
-    for(let activeArticle of activeArticles ) {
-        activeArticle.classList.remove('active');
-}
+  
+    for(let activeArticle of activeArticles){
+      activeArticle.classList.remove('active');
+    }
 
     /* get 'href' attribute from the clicked link */
-   
+    const articleSelector = clickedElement.getAttribute('href');
 
     /* find the correct article using the selector (value of 'href' attribute) */
-    
+    const selectedArticle = document.querySelector(articleSelector);
 
     /* add class 'active' to the correct article */
-    
+    selectedArticle.classList.add('active');
 }
 
 
