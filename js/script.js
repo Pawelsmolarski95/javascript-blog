fbadf8a70f4248902a3bfc09e1a809f61e092ee5
Pawel Smolarski <smolenc1@gmail.com>
2022-08-11 19:40:32 +0200

    javascript-blog

--- js/script.js
+++ js/script.js
@@ -14,37 +14,37 @@ const titleClickHandler = function(event){
     event.preventDefault();
     const clickedElement = this;
     
-    /* [DONE] remove class 'active' from all article links  */
+      /* [DONE] remove class 'active' from all article links  */
 
-    const activeLinks = document.querySelectorAll('.titles a.active');
+      const activeLinks = document.querySelectorAll('.titles a.active');
 
-    for(let activeLink of activeLinks){
-    activeLink.classList.remove('active');
-    }
+      for(let activeLink of activeLinks){
+        activeLink.classList.remove('active');
+      }
 
-    /* [DONE] add class 'active' to the clicked link */
+      /* [DONE] add class 'active' to the clicked link */
 
-    clickedElement.classList.add('active');
+      clickedElement.classList.add('active');
+      console.log(clickedElement);
+
+      /* [DONE] remove class 'active' from all articles */
+
+      const activeArticles = document.querySelectorAll('.posts .active');
     
+      for(let activeArticle of activeArticles){
+        activeArticle.classList.remove('active');
+      }
 
-    /* [DONE] remove class 'active' from all articles */
+      /* [DONE] get 'href' attribute from the clicked link */
 
-    const activeArticles = document.querySelectorAll('.posts .active');
-  
-    for(let activeArticle of activeArticles){
-      activeArticle.classList.remove('active');
-    }
+      const articleSelector = clickedElement.getAttribute('href');
 
-    /* [DONE] get 'href' attribute from the clicked link */
+      /* [DONE] find the correct article using the selector (value of 'href' attribute) */
 
-    const articleSelector = clickedElement.getAttribute('href');
+      const targetArticle = document.querySelector(articleSelector);
 
-    /* [DONE] find the correct article using the selector (value of 'href' attribute) */
-
-    const targetArticle = document.querySelector(articleSelector);
-
-    /* add class 'active' to the correct article */
-    targetArticle.classList.add('active');
+      /* add class 'active' to the correct article */
+      targetArticle.classList.add('active');
 };
 
 
